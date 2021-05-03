dd1bc8740d4424cdedd082ae1c92ed4591e927a8
long le <lenguyenbaolong@yahoo.com>
2021-05-03 16:41:32 -0700

    more clean up , added firebase config

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Dev-Space React Project ( FaceBook clone)
+
+
+
 
 ## Available Scripts
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAB4VUCTxVhzDyJB9_gZc8G8E9X4euyPVk",
+    authDomain: "devspace-b8eab.firebaseapp.com",
+    projectId: "devspace-b8eab",
+    storageBucket: "devspace-b8eab.appspot.com",
+    messagingSenderId: "954128770314",
+    appId: "1:954128770314:web:44028e8146675ea41146f3",
+    measurementId: "G-E50HQRHKY6"
+};
